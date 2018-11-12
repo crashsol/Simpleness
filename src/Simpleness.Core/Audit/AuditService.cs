dc44da617736885c8d7f36a6f54c908eda1e5c37
Crashsol <47147551@qq.com>
2018-11-12 17:43:16 +0800

    修改审计日志

---
+++ src/Simpleness.Core/Audit/AuditService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Simpleness.DataEntityFramework;
+using Simpleness.DataEntityFramework.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Simpleness.Core.Audit
+{
+    public class AuditService : BaseService,IAuditService
+    {
+
+        public AuditService(SimplenessDbContext dbContext, ILogger<AuditService> logger,
+           IMapper mapper) : base(dbContext, logger, mapper){           
+
+        }
+        public async Task CreateAsync(DataEntityFramework.Entity.Audit enity)
+        {
+            await _dbContent.Audits.AddAsync(enity);
+            await _dbContent.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<DataEntityFramework.Entity.Audit>> GetAuditLogAsync()
+        {
+           return await _dbContent.Audits.AsNoTracking().ToListAsync();
+        }
+    }
+}
---
+++ src/Simpleness.Core/Audit/IAuditService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Simpleness.DataEntityFramework.Entity;
+
+
+namespace Simpleness.Core.Audit
+{
+    public interface IAuditService
+    {
+        /// <summary>
+        /// 添加日志记录
+        /// </summary>
+        /// <param name="enity"></param>
+        /// <returns></returns>
+        Task CreateAsync(DataEntityFramework.Entity.Audit enity);
+
+
+        /// <summary>
+        /// 获取审计日志
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<DataEntityFramework.Entity.Audit>> GetAuditLogAsync();
+    }
+}
